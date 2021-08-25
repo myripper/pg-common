3f922a78f2871d07d64fed20d203e28872b3232d
myripper <myripper@gmail.com>
2021-08-25 19:49:29 +0700

    Initial commit

---
+++ Models/Stats/StatsContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PgCommon.Models.Stats;
+
+namespace PgCommon.Context
+{
+    public partial class StatsContext : DbContext
+    {
+        public StatsContext()
+        {
+        }
+
+        public StatsContext(DbContextOptions<StatsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<StreamAggregatedTrade> StreamAggregatedTrade { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StreamAggregatedTrade>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("stream_aggregated_trade");
+
+                entity.Property(e => e.Event).HasColumnType("date");
+
+                entity.Property(e => e.EventTime).HasColumnType("date");
+
+                entity.Property(e => e.Price).HasColumnType("numeric(10,7)");
+
+                entity.Property(e => e.Quantity).HasColumnType("numeric(10,7)");
+
+                entity.Property(e => e.Symbol).HasMaxLength(50);
+
+                entity.Property(e => e.TradeTime).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Stats/StreamAggregatedTrade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PgCommon.Models.Stats
+{
+    public partial class StreamAggregatedTrade
+    {
+        public long? AggregateTradeId { get; set; }
+        public bool? BuyerIsMaker { get; set; }
+        public DateTime? Event { get; set; }
+        public DateTime? EventTime { get; set; }
+        public long? FirstTradeId { get; set; }
+        public bool? Ignore { get; set; }
+        public long? LastTradeId { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Quantity { get; set; }
+        public string Symbol { get; set; }
+        public DateTime? TradeTime { get; set; }
+    }
+}
